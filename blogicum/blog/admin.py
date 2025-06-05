8be024a71cec708f7baa665202b76e703f21ba9b
Тимофей Васильев <thxog@Timofejs-MacBook-Pro.local>
2025-06-05 16:33:36 +0300

    commit3

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Category, Location, Post
+from .models import Category, Location, Post, Comment
 
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Post)
+
+@admin.register(Post, Location, Category, Comment)
+class CustomAdmin(admin.ModelAdmin):
+    pass
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,12 +1,22 @@
-from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def about(request: HttpRequest) -> HttpResponse:
-    """Представление страницы о сайте."""
-    return render(request, 'pages/about.html')
+class AboutView(TemplateView):
+    template_name = 'pages/about.html'
 
 
-def rules(request: HttpRequest) -> HttpResponse:
-    """Представление страницы правил."""
-    return render(request, 'pages/rules.html')
+class RulesView(TemplateView):
+    template_name = 'pages/rules.html'
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def error_500(request):
+    return render(request, 'pages/500.html', status=500)
